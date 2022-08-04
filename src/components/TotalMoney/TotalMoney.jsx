1a7fc83fbf8a039535a5130a6b879198a7869fed
Igor Garcia <igorgg@gmail.com>
2022-08-04 18:35:22 -0300

    last commit

--- src/components/TotalMoney/TotalMoney.jsx
+++ src/components/TotalMoney/TotalMoney.jsx
@@ -1,19 +1,16 @@
-import "./totalMoney.css";
+import './totalMoney.css';
 
 function TotalMoney({ listTransactions }) {
-	const allMoney = listTransactions.reduce(
-		(acc, { value }) => acc + +value,
-		0
-	);
-	return (
-		<section className="section_total">
-			<div className="totalValue">
-				<h3>Valor Total:</h3>
-				<h3 className="value">R$ {allMoney}</h3>
-			</div>
-			<span className="section_span">O valor referente ao saldo</span>
-		</section>
-	);
+  const allMoney = listTransactions.reduce((acc, { value }) => acc + +value, 0);
+  return (
+    <section className='section_total'>
+      <div className='totalValue'>
+        <h3>Valor Total:</h3>
+        <h3 className='value'>R$ {allMoney}</h3>
+      </div>
+      <span className='section_span'>O valor referente ao saldo</span>
+    </section>
+  );
 }
 
 export default TotalMoney;
